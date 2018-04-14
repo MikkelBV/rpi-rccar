0d3eca03016d9d98d7e6db887b2ce942df400cd1
Mikkel Brogaard Vittrup <mikkel.b.vittrup@gmail.com>
2018-04-14 11:55:12 +0200

    renamed some variables

--- server/MotorDriver.py
+++ server/MotorDriver.py
@@ -5,32 +5,30 @@ import smbus
 bus = smbus.SMBus(1)
 
 class MotorDriver:
-    MotorSpeedSet	= 0x82
-    PwMFrequenceSet	= 0x84
-    DirectionSet	= 0xaa
-    MotorSetA		= 0xa1
-    MotorSetB		= 0xa5
-    Nothing		= 0x01
-    EnableStepper	= 0x1a
-    UnenableStepper	= 0x1b
-    Stepernu		= 0x1c
-    I2CMotorDriverAdd	= 0x0f
+    MOTOR_SPEED_SET	    = 0x82
+    PWM_FREQUENCY_SET	= 0x84
+    DIRECTION_SET	    = 0xaa
+    MOTOR_SET_A		    = 0xa1
+    MOTOR_SET_B		    = 0xa5
+    NOTHING		        = 0x01
+    ENABLE_STEPPER	    = 0x1a
+    DISABLE_STEPPER	    = 0x1b
 
     def __init__(self, address = 0x0f):
         self.I2CMotorDriverAdd = address
 
-    def map_vals(self, value, leftMin, leftMax, rightMin, rightMax):
+    def _map_vals(self, value, leftMin, leftMax, rightMin, rightMax):
         leftSpan = leftMax - leftMin
         rightSpan = rightMax - rightMin
         valueScaled = float(value - leftMin / float(leftSpan))
         return int(rightMin + (valueScaled * rightSpan))
 
-    def MotorSpeedSetAB(self, MotorSpeedA, MotorSpeedB):
-        MotorSpeedA = self.map_vals(MotorSpeedA, 0, 100, 0, 255)
-        MotorSpeedB = self.map_vals(MotorSpeedB, 0, 100, 0, 255)
-        bus.write_i2c_block_data(self.I2CMotorDriverAdd, self.MotorSpeedSet, [MotorSpeedA, MotorSpeedB])
+    def motor_speed_set(self, MotorSpeedA, MotorSpeedB):
+        MotorSpeedA = self._map_vals(MotorSpeedA, 0, 100, 0, 255)
+        MotorSpeedB = self._map_vals(MotorSpeedB, 0, 100, 0, 255)
+        bus.write_i2c_block_data(self.I2CMotorDriverAdd, self.MOTOR_SPEED_SET, [MotorSpeedA, MotorSpeedB])
         time.sleep(.02)
 
-    def MotorDirectionSet(self, direction):
-        bus.write_i2c_block_data(self.I2CMotorDriverAdd, self.DirectionSet, [direction, 0])
+    def motor_direction_set(self, direction):
+        bus.write_i2c_block_data(self.I2CMotorDriverAdd, self.DIRECTION_SET, [direction, 0])
         time.sleep(.02)
--- server/server.py
+++ server/server.py
@@ -4,13 +4,15 @@ from MotorDriver import MotorDriver
 motor = MotorDriver()
 
 class Socket (WebSocket):
+    def handleConnected(self):
+        print('device connected')
+        motor.motor_direction_set(0b1010)
+
     def handleMessage(self):
         # echo message back to client
         data = str(self.data)
         (left, right) = data.split(':')
-        print(data)
-        motor.MotorSpeedSetAB(int(left), int(right))
-        motor.MotorDirectionSet(0b1010)
+        motor.motor_speed_set(int(left), int(right))
 
 
 if __name__ == '__main__':
